af3c24aea984ecf54410d49b96eaa66f34d1e42f
Felipe Reina <110634745+JFREINAP@users.noreply.github.com>
2022-09-20 11:44:59 -0500

    Update Ejercicios 03 recursion.py

--- Ejercicios 03 recursion.py
+++ Ejercicios 03 recursion.py
@@ -32,3 +32,59 @@ def f_fibonacci(n):
 
 f_fibonacci(12)
 
+
+## Ejercicio objetos.
+# TODO: 1. Cree un archivo, Taller donde llevará acabo el codigo principal
+#       crear una lista de vehiculos que se encuentran en el taller en ese momento que tienen asociados: fecha de entrega, costo, modelo, año y dueño
+#       crear una caja donde se almacena el dinero y se lleva registro de los movimientos
+#       crear una lista de empleados que tienen asociado un nombre, cargo, salario, vehiculo y documento
+#       Crear una lista de clientes que tienen asociado un nombre, vehiculo y documento
+
+# TODO: 2 Ejecute sobre so codigo principal:
+#       ingresar un vehiculo nuevo al taller (validar cupo)
+#       Sacar un vehiculo del taller (se debe generar un pago por parte del cliente)
+#       PAgar a los empleados (se debe hacer una reducción en el dinero de la caja)
+#       Contrarar a una persona nueva
+#       Despedir a una persona
+#       Encontrar un vehiculo a partir de su dueño y determinar si está en el taller
+# TODO: 3 Todas las funciones que implemente deben estar en un script distinto al principal y donde se definen sus clases
+#       NOTA - (la UNICA exepción son las funciones que definen dentro de sus clases.)
+
+
+class Taller:
+    def __init__(self, fecha, costo, modelo, año, dueño):
+        self.fecha = fecha
+        self.costo = costo
+        self.modelo = modelo
+        self.año = año
+        self.dueño = dueño
+        self.vehiculo=self
+    def caja(self, din):
+        dinerocaja=199000000
+        dinerocaja+=din
+        listavacia=[]
+        if din >= self.costo:
+            tot = din - self.costo
+            print("se ha eliminado el vehiculo")
+            dinerocaja-=tot
+            listavacia.append(tot)
+    def empleados (self, nombree, cargoe, salarioe, vehiculoe, documentoe):
+        self.nombree=nombree
+        self.cargoe=cargoe
+        self.salarioe=salarioe
+        self.vehiculoe=vehiculoe
+        self.documentoe=documentoe
+    def procesos(self,despedirocontratar,nombre1,cargo1,salario1,vehiculo1,documento1):
+        listavacia1= []
+        if procesos=='despedir':
+            ident=input('documento del empleado')
+            for i in listavacia1:
+                if i==ident:
+                    listavacia1.remove[i]
+
+'vehiculos'
+
+carro1 = Taller('24-5-02', 45000000, 'Nissan', 1993, 'Miguel Rodriguez')
+carro2 = Taller('31-10-01', 6660000, 'Ford', 1960, 'Cristofer Cruz')
+carro1.caja(46000000)
+
